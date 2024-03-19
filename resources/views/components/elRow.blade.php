71d6af156424b437ed34064218ca2595da5c30df
Dieze Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2024-03-20 06:30:27 +0700

    cicdznutz

--- resources/views/components/elRow.blade.php
+++ resources/views/components/elRow.blade.php
@@ -1,5 +1,5 @@
 <tr>
-    <td scope="row" style="background: url('{{$product->firstImage()}}'); background-size:cover">{{$i++}}</td>
+    <td scope="row" style="background: url('{{$product->firstImage()}}'); background-size:cover"><span class="sr-only">{{$i++}}</span></td>
     <td><a href="{{$product->getURL()}}">{{$product->name}} {!! $product->dispKind(true) !!}{!! $product->feature_flag ? '<span>⭐</span>' : "" !!}</a></td>
     <td class="text-right">{{$product->displayPrice()}}</td>
 </tr>
