0bf39aa0f26be290fb92a25ecbf888699a0747f4
Dieze, Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2024-03-20 01:30:40 +0700

    abdul

--- resources/views/k4/menu.blade.php
+++ resources/views/k4/menu.blade.php
@@ -101,6 +101,7 @@ $u1 = \Grassstation\Models\Product::getU1();$u2 = \Grassstation\Models\Product::
       </div>
     </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js"></script>
 <script>
 $(document).ready(function () {
     $("html, body").animate({
