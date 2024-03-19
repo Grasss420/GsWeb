438f4b94f6ddf39aaa069920629756c5f5f51faf
Dieze Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2024-03-20 00:12:40 +0700

    menumynig

--- resources/views/k4/menu.blade.php
+++ resources/views/k4/menu.blade.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link href="//static.monolidthz.com/generic/css/HelveticaNeueLTCom_ChromeCompat.css" rel="stylesheet" />
-  <title>ร้านขายกัญชา Grassstation สาขาคลองสี่</title>
+  <title>เมนูกัญชา Grassstation สาขาคลองสี่</title>
 	<style>
 		.footer { background-color: #f5f5f5; padding-top:10px;}
 		.bg-grasss{background:#507c58;color:#d7d7d5;}
@@ -21,21 +21,20 @@
 </nav>
   <div class="container my-5">
 	  <h1 class="text-center">Grassstation <small>Khlong 4</small></h1>
-    <p class="text-center"><a href="{{route('k4.menu')}}">เมนูกัญชาสาขาคลองสี่</a></p>
-    <p class="text-center">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3871.2063406924312!2d100.6870689!3d14.005776!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d7f200c78b6f3%3A0x1e62576f950a0a56!2sGrassstation!5e0!3m2!1sth!2sth!4v1707335522938!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></iframe>
-	  </p>
-	  <p class="text-center">
-		  <a href="https://maps.app.goo.gl/aMcRvoxFuBJHhZVQA" target="_blank" class="btn btn-success">Google Maps</a>
-	  </p>
+@php
+//get featured menu
+$fp = \Grassstation\Models\Product::Featured()->OrderByDesc("price_per_gram")->OrderBy("internal_sku")->get();
+@endphp
+    <p class="text-center">Website อยู่ในระหว่างการก่อสร้าง</p>
+    
   </div>
     <footer class="footer">
       <div class="container">
         <span class="text-muted">Copyright&copy; 2022-2024 <a href="http://https://wiki.spkz.monolidthz.com/Grasss_Network" target="_blank" rel="noopener noreferrer">Grasss Network</a>. &nbsp; | &nbsp;</span>
 		  <ul class="list-inline" style="display: inline-block;">
-			  <li class="list-inline-item"><a href="https://grassstation.xyz">สาขาหลัก</a></li>
-			  <li class="list-inline-item"><a href="https://k2.grassstation.xyz">สาขาคลองสอง</a></li>
-			  <li class="list-inline-item"><a href="https://k4.grassstation.xyz">สาขาคลองสี่</a></li>
+			  <li class="list-inline-item"><a href="{{route('root')}}">สาขาหลัก</a></li>
+			  <li class="list-inline-item"><a href="#">สาขาคลองสอง</a></li>
+			  <li class="list-inline-item"><a href="{{route('k4.root')}}">สาขาคลองสี่</a></li>
 			  <!--<li class="list-inline-item"><a href="https://grass.whiteglassware.net">สาขาบางกรวย</a></li>-->
 			</ul>
       </div>
