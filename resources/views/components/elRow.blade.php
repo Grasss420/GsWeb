942d8be0273992e99b88371388809ba794c39b03
Dieze, Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2024-03-20 20:09:40 +0700

    e

--- resources/views/components/elRow.blade.php
+++ resources/views/components/elRow.blade.php
@@ -1,4 +1,4 @@
-<tr>
+<tr class="{{$product->feature_flag ? 'warning' : ''}}">
     <td scope="row" style="background: url('{{$product->firstImage()}}'); background-size:cover"><span class="sr-only">{{$i++}}</span></td>
     <td><a href="{{$product->getURL()}}">{{$product->name}} {!! $product->dispKind(true) !!}{!! $product->feature_flag ? '<span>⭐</span>' : "" !!}</a></td>
     <td class="text-right">{{$product->displayPrice()}}</td>
