db58bd422feb46c0849fa0aaad51c83c097527f0
Dieze, Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2024-03-20 21:03:55 +0700

    meow

--- resources/views/k4/menu.blade.php
+++ resources/views/k4/menu.blade.php
@@ -2,6 +2,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="refresh" content="60">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css">
   <link href="//static.monolidthz.com/generic/css/HelveticaNeueLTCom_ChromeCompat.css" rel="stylesheet" />
   <link rel="stylesheet" href="{{asset('css/win95.css')}}">
