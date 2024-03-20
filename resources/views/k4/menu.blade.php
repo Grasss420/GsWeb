d9d80f61c82134fcb29fff5666789d76e4eee792
Dieze, Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2024-03-20 21:51:21 +0700

    fs

--- resources/views/k4/menu.blade.php
+++ resources/views/k4/menu.blade.php
@@ -118,6 +118,9 @@ $u1 = \Grassstation\Models\Product::getU1();$u2 = \Grassstation\Models\Product::
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js"></script>
 <script>$(document).ready(function () {
+    if (/Android/.test(navigator.userAgent)) {
+        document.documentElement.requestFullscreen();
+    }
     // Set scroll position to 0
     $("html, body").animate({
         scrollTop: 0
