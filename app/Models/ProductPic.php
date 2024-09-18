460b15d935ba1489fa2636ce7ed8c48e5c25b120
Dieze Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2024-09-17 23:19:33 -0700

    dogsit

--- app/Models/ProductPic.php
+++ app/Models/ProductPic.php
@@ -5,6 +5,13 @@ namespace Grassstation\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @author  MoNolidThZ - SPKZ Design Co. <admin@monolidthz.com>
+ * @version 1.0.0
+ * @since 1.0.0 (Creation Time Unknown)
+ * 
+ * Picjer of a product image
+ */
 class ProductPic extends Model
 {
     use HasFactory;
@@ -17,6 +24,9 @@ class ProductPic extends Model
         }
     }
 
+    /**
+     * Return a public URL of image link
+     */
     public function getURL()
     {
         if(str_starts_with($this->url, "public/gsi/")){
@@ -31,12 +41,19 @@ class ProductPic extends Model
         return '<img src="'.$this->getURL().'">';
     }
 
+    /**
+     * Internal function to store files
+     */
     public static function storeFile($path)
     {
         $isf = new self(["url"=>$path]);
         $isf->save();
         return $isf;
     }
+
+    /**
+     * Query Default Sorting
+     */
     public static function qdfs(){
         return static::query()->orderByDesc('id');
     }
