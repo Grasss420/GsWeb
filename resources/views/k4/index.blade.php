c19d5844d393e41418f717ebe9f635e9a4d02bed
Dieze Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2025-02-24 02:09:11 +0700

    K4 Page EOL

--- resources/views/k4/index.blade.php
+++ resources/views/k4/index.blade.php
@@ -23,10 +23,10 @@
 	  <h1 class="text-center">Grassstation <small>Khlong 4</small></h1>
     <p class="text-center"><a href="{{route('k4.menu')}}">เมนูกัญชาสาขาคลองสี่</a></p>
     <p class="text-center">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5609.204109957495!2d100.68864714001836!3d14.003772966715434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d7fb8ac001a99%3A0x9641937dd3c3c263!2zR3Jhc3NzdGF0aW9uIOC4o-C5ieC4suC4meC4guC4suC4ouC4geC4seC4jeC4iuC4siDguJvguLLguIHguITguKXguK3guIc0IChDYW5uYWJpcyBTdG9yZSk!5e0!3m2!1sen!2sth!4v1731492382926!5m2!1sen!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d968.1892969645776!2d100.6881947695446!3d14.017708900318395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d7f561c973403%3A0x45b82de71f304876!2zR3JlZW5MYW5kIOC4o-C5ieC4suC4meC4guC4suC4ouC4geC4seC4jeC4iuC4siDguJXguKXguLLguJTguYDguKHguLfguK3guIfguYPguKvguKHguYjguITguKXguK3guIc0IC0gQ2FubmFiaXMgc3RvcmU!5e0!3m2!1sen!2sth!4v1740332783175!5m2!1sen!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 	  </p>
 	  <p class="text-center">
-		  <a href="https://maps.app.goo.gl/wHvbtacVQwYiP1ES8" target="_blank" class="btn btn-success">Google Maps</a>
+		  <a href="https://maps.app.goo.gl/sPDzEkMSeHSyeU477" target="_blank" class="btn btn-success">Google Maps</a>
 	  </p>
   </div>
     <footer class="footer">
@@ -34,9 +34,9 @@
         <span class="text-muted">Copyright&copy; 2022-2024 <a href="http://https://wiki.spkz.monolidthz.com/Grasss_Network" target="_blank" rel="noopener noreferrer">Grasss Network</a>. &nbsp; | &nbsp;</span>
 		  <ul class="list-inline" style="display: inline-block;">
 			  <li class="list-inline-item"><a href="https://grassstation.xyz">สาขาหลัก</a></li>
-			  <li class="list-inline-item"><a href="https://k2.grassstation.xyz">สาขาคลองสอง</a></li>
+			  <!--<li class="list-inline-item"><a href="https://k2.grassstation.xyz">สาขาคลองสอง</a></li>-->
 			  <li class="list-inline-item"><a href="https://k4.grassstation.xyz">สาขาคลองสี่</a></li>
-        <li class="list-inline-item"><a href="https://galactichigh.grassstation.xyz/">สาขารังสิต</a></li>
+        <li class="list-inline-item"><a href="https://galactichigh.grassstation.xyz/"><del>สาขารังสิต</del></a></li>
 			  <!--<li class="list-inline-item"><a href="https://grass.whiteglassware.net">สาขาบางกรวย</a></li>-->
 			</ul>
       </div>
