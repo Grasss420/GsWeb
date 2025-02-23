3e94d8500e2c7dd4ba2256cc3b841fd886472348
Dieze Nutz <5730242+MoNoLidThZ@users.noreply.github.com>
2025-02-24 02:12:02 +0700

    EOL K4x

--- resources/views/k4/index.blade.php
+++ resources/views/k4/index.blade.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link href="//static.monolidthz.com/generic/css/HelveticaNeueLTCom_ChromeCompat.css" rel="stylesheet" />
-  <title>ร้านขายกัญชา Grassstation สาขาคลองสี่</title>
+  <title>ร้านขายกัญชา GreenLand สาขาตลาดคลองสี่เมืองใหม่</title>
 	<style>
 		.footer { background-color: #f5f5f5; padding-top:10px;}
 		.bg-grasss{background:#507c58;color:#d7d7d5;}
@@ -16,22 +16,23 @@
     <img src="https://grassstation.xyz/img/gs.png" height="40" class="d-inline-block align-top" alt="Grassstation Logo">
   </a>
     <span class="navbar-text">
-	  Grassstation Khlong 4
+	  GreenLand Khlong 4
     </span>
 </nav>
   <div class="container my-5">
-	  <h1 class="text-center">Grassstation <small>Khlong 4</small></h1>
+	  <h1 class="text-center">GreenLand <small>Khlong 4</small></h1>
     <p class="text-center"><a href="{{route('k4.menu')}}">เมนูกัญชาสาขาคลองสี่</a></p>
     <p class="text-center">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d968.1892969645776!2d100.6881947695446!3d14.017708900318395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d7f561c973403%3A0x45b82de71f304876!2zR3JlZW5MYW5kIOC4o-C5ieC4suC4meC4guC4suC4ouC4geC4seC4jeC4iuC4siDguJXguKXguLLguJTguYDguKHguLfguK3guIfguYPguKvguKHguYjguITguKXguK3guIc0IC0gQ2FubmFiaXMgc3RvcmU!5e0!3m2!1sen!2sth!4v1740332783175!5m2!1sen!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 	  </p>
 	  <p class="text-center">
-		  <a href="https://maps.app.goo.gl/sPDzEkMSeHSyeU477" target="_blank" class="btn btn-success">Google Maps</a>
+		  <a href="https://maps.app.goo.gl/sPDzEkMSeHSyeU477" target="_blank" class="btn btn-info">Google Maps</a>
+		  <a href="https://line.me/R/ti/p/@884ccqbw" target="_blank" class="btn btn-success">Contact LINE</a>
 	  </p>
   </div>
     <footer class="footer">
       <div class="container">
-        <span class="text-muted">Copyright&copy; 2022-2024 <a href="http://https://wiki.spkz.monolidthz.com/Grasss_Network" target="_blank" rel="noopener noreferrer">Grasss Network</a>. &nbsp; | &nbsp;</span>
+        <span class="text-muted">Copyright&copy; 2022-{{date('Y')}} <a href="http://https://sarkhan.wiki/Grasss_Network" target="_blank" rel="noopener noreferrer">Grasss Network</a>. &nbsp; | &nbsp;</span>
 		  <ul class="list-inline" style="display: inline-block;">
 			  <li class="list-inline-item"><a href="https://grassstation.xyz">สาขาหลัก</a></li>
 			  <!--<li class="list-inline-item"><a href="https://k2.grassstation.xyz">สาขาคลองสอง</a></li>-->
